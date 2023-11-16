028eec278c7ac3181c20ad97ab865bd57fe4cd2c
Mehradi <mehradi.info@gmail.com>
2023-11-16 18:42:01 +0100

    React.memo - rendering optimisation in React

---
+++ src/CheckMemo.tsx
@@ -0,0 +1,31 @@
+import { memo, useState } from "react";
+
+const CheckMemo = () => {
+  const [name, setName] = useState("");
+  const [address, setAddress] = useState("");
+  return (
+    <>
+      <label>
+        Name{": "}
+        <input value={name} onChange={(e) => setName(e.target.value)} />
+      </label>
+      <label>
+        Address{": "}
+        <input value={address} onChange={(e) => setAddress(e.target.value)} />
+      </label>
+      <Greeting name={name} />
+    </>
+  );
+};
+
+const Greeting = memo(function Greeting({ name }) {
+  console.log("Greeting was rendered at", new Date().toLocaleTimeString());
+  return (
+    <h3>
+      Hello{name && ", "}
+      {name}!
+    </h3>
+  );
+});
+
+export default CheckMemo;
